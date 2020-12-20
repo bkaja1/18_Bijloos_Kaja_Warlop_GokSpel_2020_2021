1b7eea22e2271dcdea2271c852f4c1e160057bc9
niels-bijloos <55435391+niels-bijloos@users.noreply.github.com>
2020-12-20 17:05:56 +0100

    Merge branch 'master' of C:\Users\niels\Documents\OO Ontwerpen\18_Bijloos_Kaja_Warlop_GokSpel_2020_2021 with conflicts.

--- src/controller/StatistiekController.java
+++ src/controller/StatistiekController.java
@@ -1,11 +1,39 @@
 package controller;
 
+import model.Spel;
+import model.gokstrategies.GokStrategy;
+
 import view.panels.StatistiekPane;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class StatistiekController {
+public class StatistiekController implements WaitObserver, GameObserver{
 
-    private StatistiekPane statistiekPane;
-    private ArrayList<>
+    private StatistiekPane view;
+    private Spel spel;
+
+    private GamblerController gamblerController;
+
+    private HashMap<GokStrategy, ArrayList<Double>> statistieken;
+
+    public StatistiekController(Spel spel) {
+        this.spel = spel;
+        spel.addWaitObserver(this);
+        spel.addGameObserver(this);
+    }
+
+    public void setView(StatistiekPane view) {
+        this.view = view;
+    }
+
+    @Override
+    public void updateGame(Object object) {
+
+    }
+
+    @Override
+    public void updateWait(String wait) {
+
+    }
 }
